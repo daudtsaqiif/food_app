cd2a14f9922a83f7dd5a0e3b08cd727974e2ef4a
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-09-12 14:58:34 +0700

    1

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,5 +1,23 @@
 part of 'models.dart';
 
 class User {
-  
+  final int? id;
+  final String? name;
+  final String? email;
+  final String? addrees;
+  final String? houseNumber;
+  final String? phoneNumber;
+  final String? city;
+  final String? picturePath;
+
+  User({
+    this.id,
+    this.name,
+    this.email,
+    this.addrees,
+    this.houseNumber,
+    this.phoneNumber,
+    this.city,
+    this.picturePath,
+  });
 }
