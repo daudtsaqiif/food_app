bb88a9dcf0ed249fcdbc9775537c7695a2e444b1
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-09-16 10:44:50 +0700

    1

--- lib/ui/detail_page.dart
+++ lib/ui/detail_page.dart
@@ -15,10 +15,10 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
-      int quantity = 1;
+  int quantity = 1;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  
+    return Scaffold(
       body: Stack(
         children: [
           Container(
@@ -29,6 +29,7 @@ class _DetailPageState extends State<DetailPage> {
               color: Colors.white,
             ),
           ),
+          //gambar food
           SafeArea(
             child: Container(
               width: double.infinity,
@@ -45,6 +46,7 @@ class _DetailPageState extends State<DetailPage> {
           SafeArea(
             child: ListView(
               children: [
+                //arrow_back
                 Container(
                   height: 100,
                   padding: EdgeInsets.symmetric(
@@ -73,6 +75,7 @@ class _DetailPageState extends State<DetailPage> {
                     ),
                   ),
                 ),
+                //container melengkung
                 Container(
                   margin: EdgeInsetsDirectional.only(top: 180),
                   padding: EdgeInsets.symmetric(vertical: 26, horizontal: 16),
@@ -83,10 +86,13 @@ class _DetailPageState extends State<DetailPage> {
                       ),
                       color: Colors.white),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                      //name food and rating + quantity food
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
+                          //name food and rating
                           SizedBox(
                             width: MediaQuery.of(context).size.width * 0.5,
                             child: Column(
@@ -156,7 +162,87 @@ class _DetailPageState extends State<DetailPage> {
                         ],
                       ),
                       //food description
-                      Container(),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 14, 0, 16),
+                        child: Text(
+                          widget.food!.description!,
+                          style: blackFontStyle3,
+                          textAlign: TextAlign.justify,
+                        ),
+                      ),
+                      //komposisi
+                      Row(
+                        children: [
+                          Text(
+                            'Komposisi ',
+                            style: blackFontStyle2,
+                          ),
+                          Icon(
+                            Icons.info,
+                            color: mainColor,
+                            size: 20,
+                          ),
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 4, 0, 16),
+                        child: Text(widget.food!.ingredients!),
+                      ),
+                      //harga
+                      Container(
+                        margin: EdgeInsets.only(bottom: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  'Total price ',
+                                  style: blackFontStyle2,
+                                ),
+                                Icon(
+                                  Icons.monetization_on,
+                                  color: mainColor,
+                                  size: 20,
+                                ),
+                              ],
+                            ),
+                            Text(
+                              NumberFormat.currency(
+                                      symbol: 'IDR',
+                                      decimalDigits: 0,
+                                      locale: 'id_ID')
+                                  .format(
+                                quantity * widget.food!.price!,
+                              ),
+                              style: blackFontStyle2,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      //order button
+                      SizedBox(
+                        width: double.infinity,
+                        height: 45,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: mainColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              )),
+                          onPressed: () {},
+                          child: Text(
+                            'Order Now',
+                            style: blackFontStyle2.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                        ),
+                      )
                     ],
                   ),
                 ),
--- lib/ui/pages.dart
+++ lib/ui/pages.dart
@@ -6,6 +6,7 @@ import 'package:food_app/model/models.dart';
 import 'package:food_app/shared/shared.dart';
 import 'package:food_app/ui/widget/widgets.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:supercharged/supercharged.dart';
 part 'general_page.dart';
 part 'sign_in_page.dart';
